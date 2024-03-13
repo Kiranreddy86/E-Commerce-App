7ccbef2c616b01415034dc506637c27f01739d71
kiran reddy <chandrakiranreddyramana7780@gmail.com>
2024-03-13 06:33:23 +0530

    config Updated

--- User/src/main/java/com/Ecommerce/User/Controller/AuthLogin.java
+++ User/src/main/java/com/Ecommerce/User/Controller/AuthLogin.java
@@ -8,6 +8,7 @@ import com.Ecommerce.User.Service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,11 +21,13 @@ public class AuthLogin {
     AuthService authService;
 
     @PostMapping("/signIn")
+    @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
     public ResponseEntity<JwtResponse> login(@RequestBody JwtRequest jwtRequest)
     {
         return new ResponseEntity<>(authService.login(jwtRequest), HttpStatus.OK);
     }
     @PostMapping("/signUp")
+    @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
     public ResponseEntity register(@RequestBody Users user) throws UserAlreadyExistException {
         return authService.register(user);
     }
